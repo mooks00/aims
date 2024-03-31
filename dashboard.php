cb3e3cbdd0be5c33359fbe49f08277b17bba998b
czaninadrienne <czaninaadrienne@gmail.com>
2024-03-31 22:50:29 +0800

    modified dashboard

--- dashboard.php
+++ dashboard.php
@@ -16,35 +16,38 @@ $currentDate = date("Y-m-d");
 <head>
     <title>Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Dashboard</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white" style=" box-shadow: 0 2px 4px 0 rgba(0,0,0,.2)">
+    <a class="navbar-brand" href="dashboard.php">
+    <img src="logo.png" class="logo" style="padding: 5px 8px"/>
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="dashboard.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Products</a>
+                    <a class="nav-link" href="products.php">Products</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Categories</a>
+                    <a class="nav-link" href="categories.php">Categories</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Vendor</a>
+                    <a class="nav-link" href="vendor.php">Vendor</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Sales</a>
+                    <a class="nav-link" href="sales.php">Sales</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Orders</a>
+                    <a class="nav-link" href="orders.php">Orders</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Shipments</a>
+                    <a class="nav-link" href="shipments.php">Shipments</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="logout.php">Log Out</a>
@@ -57,13 +60,20 @@ $currentDate = date("Y-m-d");
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    <div class="header">
-        <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
-        <p>Today's Date: <?php echo $currentDate; ?></p>
+    <div style="background-color:#006A4E">
+    <div class="container d-flex justify-content-center align-items-center" style="height: 500px">
+        <div class="row">
+            <div class="col text-center">
+                <h1 style="color: white">Welcome, <?php echo $_SESSION['username']; ?>!</h1>
+                <p style="color: white"><i>Today's Date: <?php echo $currentDate; ?></i></p>
+            </div>
+        </div>
     </div>
-    
+
+    <!-- Optional additional content -->
     <div class="content">
         <!-- Add more content here based on your requirements -->
     </div>
+</div>
 </body>
 </html>
